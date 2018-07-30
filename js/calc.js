5b725b1a666faf2d12fa770158ca201df68673fc
NayeliGarcia <nayeli.garcia2222@gmail.com>
2018-07-30 21:20:27 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
   doMath();
 });
 
-var num = $ ('#number1');
+var num1 = $('#number1');
 var num2 = $('#number2')
 var operator = $('#operation');
 
 function doMath(){
-  $('#equals').on('click', function){
+  $('#equals').on('click', function(){
     num1 = parseFloat(num1.val());
     num2 = parseFloat(num2.val());
     operator = operator.val();
